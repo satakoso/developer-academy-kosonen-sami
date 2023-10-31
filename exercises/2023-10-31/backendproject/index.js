d86cca246f4334ff706ece4ba5f1ba5c84471528
Sami Kosonen <stkosonen@gmail.com>
2023-10-31 11:50:52 +0200

    Added lab03

--- exercises/2023-10-31/backendproject/index.js
+++ exercises/2023-10-31/backendproject/index.js
@@ -1,10 +1,44 @@
-function example(x) {
-  if (x > 10) {
-    return true;
-  }
-  return false;
-}
-const x = 4;
-console.log(example(x));
+const express = require('express');
 
-console.log('Moro');
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/randomize', (req, res) => {
+  const number1 = Math.floor(Math.random() * 3);
+  const number2 = Math.floor(Math.random() * 3);
+  const number3 = Math.floor(Math.random() * 3);
+  if (number1 === number2 && number1 === number3) {
+    res.send(
+      'number1:' +
+        number1 +
+        '\n' +
+        'number2:' +
+        number2 +
+        '\n' +
+        'number3:' +
+        number3 +
+        '\n' +
+        'succes!',
+    );
+  } else {
+    res.send(
+      'number1:' +
+        number1 +
+        '\n' +
+        'number2:' +
+        number2 +
+        '\n' +
+        'number3:' +
+        number3,
+    );
+  }
+});
+
+app.listen(port, () => {
+  console.log('Example applistening on port ${port}');
+});
