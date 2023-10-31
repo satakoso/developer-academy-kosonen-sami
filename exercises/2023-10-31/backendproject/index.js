0f8cfb5e8b318a7d42c1d25e6b45d6f01b5feb05
Sami Kosonen <stkosonen@gmail.com>
2023-10-31 14:54:38 +0200

    Added exercise 07

--- exercises/2023-10-31/backendproject/index.js
+++ exercises/2023-10-31/backendproject/index.js
@@ -5,10 +5,17 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 // res.send('Hello World!');
 // });
+let id = 0;
+let db = [
+  { id: ++id, latitude: 60, longitude: 60 },
+  { id: ++id, latitude: 62, longitude: 63 },
+  { id: ++id, latitude: 65, longitude: 63 },
+];
 
 app.get('/randomize', (req, res) => {
   const number1 = Math.floor(Math.random() * 3);
@@ -40,10 +47,32 @@ app.get('/randomize', (req, res) => {
     );
   }
 });
-
 app.get('/locations', (req, res) => {
-  res.sendFile(__dirname + '/locations.html');
+  res.json(db);
+  // res.send('Getting all locations');
 });
+app.get('/locations/:muuttuja([0-9]+)', (req, res) => {
+  const id = parseInt(req.params.muuttuja);
+  const location = db.find((item) => item.id === id);
+  res.json(location);
+  // res.send('Getting alocation with id: ' + id);
+});
+app.post('/locations', (req, res) => {
+  const location = req.body;
+  location.id = ++id;
+  // console.log(location);
+  db.push(location);
+  // res.send('Adding new location');
+});
+app.delete('/locations/:muuttuja([0-9]+)', (req, res) => {
+  const id = parseInt(req.params.muuttuja);
+  db = db.filter((item) => item.id !== id);
+  // res.send('Delete location with id:' + id);
+});
+
+//app.get('/locations', (req, res) => {
+//  res.sendFile(__dirname + '/locations.html');
+// });
 
 app.listen(port, () => {
   console.log('Example applistening on port ${port}');
